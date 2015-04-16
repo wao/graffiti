eabe22d4b92d4fde3e262fca4385a143ac63cf64
Yang Chen <mike.c.yang@gmail.com>
2015-04-16 14:15:24 +0800

    Show full screen now

---
+++ graffiti/src/main/java/info/thinkmore/android/graffiti/MainActivity.java
@@ -0,0 +1,107 @@
+package info.thinkmore.android.graffiti;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import org.androidannotations.annotations.*;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity
+    extends Activity
+{
+
+    Handler handler = new Handler();
+
+    // Called at the start of the full lifetime.
+    @SuppressLint("NewApi")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Initialize activity.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        if( Build.VERSION.SDK_INT < 16 ){
+            getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
+        }
+        else{
+            getWindow().getDecorView().setOnSystemUiVisibilityChangeListener( new View.OnSystemUiVisibilityChangeListener(){
+                @Override
+                public void onSystemUiVisibilityChange(int visibility){
+                    int flags = 0;
+                    if( (visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0 ){
+                        flags |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+                    }
+
+                    if( (visibility & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) == 0 ){
+                        Log.v( "VISIBILITY", "Need to hide navigation");
+                        flags |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+                    }
+
+                    if( flags != 0 ){
+                        handler.postDelayed( new Runnable(){
+                            @Override
+                            public void run(){
+                                enableFullScreen();
+                            }
+                        }, 2000 );
+                    }
+                }
+            });
+        }
+    }
+
+    @SuppressLint("NewApi")
+    void enableFullScreen(){
+        if( Build.VERSION.SDK_INT >= 16 ){
+            getWindow().getDecorView().setSystemUiVisibility( 
+                    View.SYSTEM_UI_FLAG_FULLSCREEN 
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    );
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+       
+        enableFullScreen();
+    }
+    
+    // Called after onCreate has finished, use to restore UI state
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore UI state from the savedInstanceState.
+        // This bundle has also been passed to onCreate.
+    }
+    
+    // Called to save UI state changes at the end of the active lifecycle.
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.  
+        super.onSaveInstanceState(savedInstanceState); 
+    }
+    
+
+    @AfterViews
+    void afterViews() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater();
+        return true;
+    }
+
+}
---
+++ graffiti/src/test/java/info/thinkmore/android/graffiti/MainActivityTest.java
@@ -0,0 +1,20 @@
+package info.thinkmore.android.graffiti;
+
+import android.app.Activity;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest="./AndroidManifest.xml")
+public class MainActivityTest {
+
+  @org.junit.Test
+  public void titleIsCorrect() throws Exception {
+    Activity activity = Robolectric.setupActivity(MainActivity_.class);
+    assertTrue(activity.getTitle().toString().equals("graffiti"));
+  }
+}
