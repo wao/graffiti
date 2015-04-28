6a712c95127e5a5337792d9f07ab198248ccd887
Yang Chen <mike.c.yang@gmail.com>
2015-04-28 22:35:42 +0800

    Finish add cofoja support

--- graffiti/src/main/java/info/thinkmore/android/graffiti/DrawingView.java
+++ graffiti/src/main/java/info/thinkmore/android/graffiti/DrawingView.java
@@ -3,8 +3,8 @@ package info.thinkmore.android.graffiti;
 import java.util.Arrays;
 import java.util.List;
 
-//import lombok.Getter;
-//import lombok.Setter;
+import lombok.Getter;
+import lombok.Setter;
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -50,16 +50,16 @@ public class DrawingView extends View {
 
         int lastIndex = 0;
 
-        //@Getter @Setter int currentColor;
-        int currentColor;
+        @Getter @Setter int currentColor;
+        //int currentColor;
 
-        public void setCurrentColor(int value){
-            currentColor = value;
-        }
+        //public void setCurrentColor(int value){
+            //currentColor = value;
+        //}
 
-        public int getCurrentColor(){
-            return currentColor;
-        }
+        //public int getCurrentColor(){
+            //return currentColor;
+        //}
 
         Lines(int currentColor ){
             this.currentColor = currentColor;
